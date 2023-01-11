5e8b9b1f76376c5c33446e29c0dfa37678880042
jaycruz1218 <jcarrion@aoiths.org>
2023-01-11 12:47:21 -0500

    save

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    public Transform playerLocation;
+    public float speed;
+     bool playerInSight;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (playerInSight)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, playerLocation.transform.position, 1 * speed * Time.deltaTime);
+        }
+        else if (!playerInSight)
+        {
+
+        }
+    }
+}
