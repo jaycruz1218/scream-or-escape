5d0eb73a72bdb734da54f9346420090fd179f297
jaycruz1218 <jcarrion@aoiths.org>
2023-05-22 13:40:28 -0400

    iu

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -6,12 +6,12 @@ public class EnemyScript : MonoBehaviour
 {
     public Transform playerLocation;
     public float speed;
-     bool playerInSight = true;
+    bool playerInSight = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
@@ -20,11 +20,45 @@ public class EnemyScript : MonoBehaviour
 
         if (playerInSight)
         {
+            transform.LookAt(new Vector3(playerLocation.position.x, transform.position.y, playerLocation.position.z));
             transform.position = Vector3.MoveTowards(transform.position, playerLocation.transform.position, 1 * speed * Time.deltaTime);
         }
         else if (!playerInSight)
         {
             transform.position = transform.position;
         }
+
+        
     }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            playerInSight = true;
+            Debug.Log("Player Seen");
+            
+        }
+
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            transform.LookAt(new Vector3(playerLocation.position.x, transform.position.y, playerLocation.position.z));
+        }
+        
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            playerInSight = false;
+            Debug.Log("Player Lost");
+        }
+    }
+
+
 }
